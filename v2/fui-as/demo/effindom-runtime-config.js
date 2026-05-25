a30481a2d0099289a5180c35063250c8476de42d
zion-sati <283163728+zion-sati@users.noreply.github.com>
2026-05-25 21:35:21 +1000

    Publish demo

--- v2/fui-as/demo/effindom-runtime-config.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  const script = document.currentScript;
-  const base = script && script.src ? script.src : window.location.href;
-  window.__effindomRuntime = Object.assign({}, window.__effindomRuntime, {
-    manifestUrl: new URL('../../browser-bridge/effindom.v2.manifest.json', base).toString(),
-  });
-})();
