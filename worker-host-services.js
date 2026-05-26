ef8d585d2e1d29df8aa33f7c178f6390f05ffc37
zion-sati <283163728+zion-sati@users.noreply.github.com>
2026-05-27 00:45:06 +1000

    Publish demo

---
+++ worker-host-services.js
@@ -0,0 +1,2 @@
+"use strict";(()=>{var e={demoWorkerClock:{wallClockSinceEpochMs:{args:[],returns:"f64",implementation(){return Date.now()}}}};globalThis.__fuiWorkerHostServicesModule={...globalThis.__fuiWorkerHostServicesModule??{},demoWorkerHostServices:e};})();
+//# sourceMappingURL=worker-host-services.js.map
